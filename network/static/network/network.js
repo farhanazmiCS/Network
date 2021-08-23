6ec94b4276fcd4b1cc93d357080a5406420eb23e
Farhan Azmi <farhanazmi177@gmail.com>
2021-08-23 15:14:02 +0800

    Modified aesthetics and updated to Bootstrap 5.

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -1,7 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('strong').addEventListener('onclick', () => getProfile());
-    document.querySelector('#all-posts').addEventListener('onclick', () => feed());
+    document.querySelector('#all-posts').addEventListener('click', () => newsfeed());
 
     // Load feed as default
-    feed();
-})
+    newsfeed();
+});
+
+function newsfeed() {
+    fetch('/posts')
+    .then(response => response.json())
+    .then(post => {
+        post.forEach(loopelement)
+    });
+    document.querySelector('#make-new-post').style.display = 'block';
+}
+
+function loopelement(eachpost) {
+    // Append all items into this div
+    let div = document.createElement('div');
+    let hPoster = document.createElement('h5');
+    let hFeedpost = document.createElement('p');
+    let poster = document.createTextNode(`${eachpost.poster}`);
+    let feedpost = document.createTextNode(`${eachpost.post}`);
+    hPoster.appendChild(poster);
+    hFeedpost.appendChild(feedpost);
+    div.appendChild(hPoster);
+    div.appendChild(hFeedpost);
+}
+
+function getProfile() {
+    // TODO
+}
